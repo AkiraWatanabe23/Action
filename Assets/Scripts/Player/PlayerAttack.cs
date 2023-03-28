afbff3970967d74a0b544c387ff1cce65d5ce1fb
Akira Watanabe <a0323w@gmail.com>
2023-03-28 15:02:50 +0900

    [update] 攻撃の形式が複数あったら嬉しい

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,8 +5,31 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerAttack
 {
+    [SerializeField] private AttackType _type = AttackType.Sword;
+
     public void Init()
     {
 
     }
+
+    public void Attack()
+    {
+        //持っている武器によって分けたい
+        switch (_type)
+        {
+            case AttackType.Sword:
+                break;
+            case AttackType.Gun:
+                break;
+            case AttackType.Hammer:
+                break;
+        }
+    }
+}
+
+public enum AttackType
+{
+    Sword,
+    Gun,
+    Hammer
 }
