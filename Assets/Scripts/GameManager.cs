4dd31b17073f7ebe1b549ccaac5ee7c8d1b3edd4
Akira Watanabe <a0323w@gmail.com>
2023-03-28 13:26:28 +0900

    [add] GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance = default;
+
+    public static GameManager Instance { get => _instance; set => _instance = value; }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ManagerAttachment.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ManagerAttachment : MonoBehaviour
+{
+    private GameManager _manager = default;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        _manager.Start();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
