3f7e3d646c2e4656eb6191cbb1d47c3d0cafcf7b
Akira Watanabe <a0323w@gmail.com>
2023-04-02 15:10:38 +0900

    [add] Enemyの移動に関するクラス

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -3,12 +3,15 @@
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private EnemyHealth _health = new();
+    [SerializeField] private EnemyMove _move = new();
 
     public EnemyHealth Health { get => _health; protected set => _health = value; }
+    public EnemyMove Move { get => _move; protected set => _move = value; }
 
     private void Awake()
     {
         _health.Init();
+        _move.Init();
     }
 
     private void Start()
@@ -18,6 +21,6 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
-        
+        _move.Update();
     }
 }
---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class EnemyMove
+{
+    [SerializeField] private MoveType _type = MoveType.Normal;
+
+    public void Init()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
+
+public enum MoveType
+{
+    Normal,
+    Boss,
+}
