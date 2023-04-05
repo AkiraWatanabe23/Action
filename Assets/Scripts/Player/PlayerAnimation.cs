cb30d534dfc7f249465028448674c9bd5f1e4d2f
Akira Watanabe <a0323w@gmail.com>
2023-04-05 15:19:49 +0900

    [add] PlayerAnimation用のクラス

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PlayerAnimation
+{
+    [Header("Animationの各パラメータ")]
+    [SerializeField] private float _move = 1f;
+
+    //各Animationを関数にまとめる
+    private Animator _anim = default;
+
+    public void Init(Animator anim)
+    {
+        _anim = anim;
+    }
+
+    public void AnimIdle(bool play)
+    {
+        _anim.SetBool("Idle", play);
+    }
+
+    public void AnimMove()
+    {
+        _anim.SetFloat("Move", _move);
+    }
+
+    public void AnimAttack()
+    {
+
+    }
+
+    public void AnimDamaged()
+    {
+
+    }
+
+    public void AnimDead()
+    {
+
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,20 +6,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private PlayerMove _movement = new();
     [SerializeField] private PlayerHealth _health = new();
     [SerializeField] private PlayerAttack _attack = new();
+    [SerializeField] private PlayerAnimation _animation = new();
 
     private CharacterController _controller = default;
+    private Animator _anim = default;
 
     public PlayerMove Move { get => _movement; protected set => _movement = value; }
     public PlayerHealth Health { get => _health; protected set => _health = value; }
     public PlayerAttack Attack { get => _attack; protected set => _attack = value; }
+    public PlayerAnimation Animation { get => _animation; protected set => _animation = value; }
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
+        _anim = GetComponent<Animator>();
 
         _movement.Init(_controller, transform);
         _health.Init();
         _attack.Init(transform);
+        _animation.Init(_anim);
     }
 
     private void Update()
