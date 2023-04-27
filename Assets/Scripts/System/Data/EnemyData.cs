e1f1e350d1a233665b24335627c54763486376e0
Akira Watanabe <a0323w@gmail.com>
2023-04-27 18:10:14 +0900

    [add] 敵データのScriptableObject

---
+++ Assets/Scripts/System/Data/EnemyData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyData : ScriptableObject
+{
+    [Tooltip("視界範囲")]
+    public float SearchAngle;
+    [Tooltip("視界の距離")]
+    public float SearchDistance;
+    [Tooltip("徘徊する場所")]
+    public List<Transform> _wanderingPos;
+}
