a8895e6fa8f389021b216170dce2b40823f4c250
Akira Watanabe <a0323w@gmail.com>
2023-05-07 02:44:57 +0900

    [update] CharacterControllerの動き模索中...

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -19,7 +19,6 @@ public class PlayerMove
     private readonly float _gravity = 20f;
 
     private Quaternion _targetRotation = default;
-    private float _targetRot = 1f;
 
     public MoveType MoveType => _moveType;
 
@@ -28,7 +27,7 @@ public class PlayerMove
         _controller = con;
         _trans = trans;
 
-        _targetRot = _trans.eulerAngles.y;
+        _targetRotation = _trans.rotation;
     }
 
     public void Init(Transform trans, Rigidbody rigidbody)
@@ -54,51 +53,26 @@ public class PlayerMove
         _hol = Input.GetAxisRaw("Horizontal");
         _ver = Input.GetAxisRaw("Vertical");
 
-        _trans.Rotate(0f, _hol * _rotateSpeed, 0f);
-
         if (_controller.isGrounded)
         {
-            _moveDir = new Vector3(_hol, 0f, _ver);
-            _moveDir = _trans.TransformDirection(_moveDir);
-            _moveDir *= _moveSpeed;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _moveDir.y = _jumpPower * Time.deltaTime;
+            }
         }
         _moveDir.y -= _gravity * Time.deltaTime;
-        _controller.Move(_moveDir * Time.deltaTime);
 
+        _moveDir = new Vector3(_hol, 0f, _ver);
+        _moveDir = _trans.TransformDirection(_moveDir);
+        _moveDir *= _moveSpeed * Time.deltaTime;
 
-        //接地中なら
-        //if (_controller.isGrounded)
-        //{
+        _controller.Move(_moveDir);
 
-        //    _moveDir.x = _hol;
-        //    _moveDir.z = _ver * _moveSpeed;
-        //    _trans.Rotate(0f, _hol, 0f);
-
-        //    if (_ver < 0f)
-        //    {
-        //        //後ろ方向の入力があった場合
-        //        //_trans.rotation = Quaternion.Euler(0f, 180f, 0f);
-
-        //        var angle = _trans.eulerAngles.y;
-        //        Quaternion rot = Quaternion.AngleAxis(angle, Vector3.up);
-
-        //        _moveDir = rot * Vector3.forward * _moveSpeed * -1;
-        //    }
-
-        //    //_moveDir = Vector3.Lerp(_beforeDir, _moveDir, _moveSpeed * Time.deltaTime);
-        //    //_beforeDir = _moveDir;
-
-        //    if (Input.GetButtonDown("Jump"))
-        //    {
-        //        _moveDir.y = _jumpPower;
-        //    }
-        //}
-        ////CharacterController.Move()には重力がないため動的にかける
-        //_moveDir.y -= _gravity * Time.deltaTime;
-
-        ////ローカル座標をワールド座標に変換する
-        //Vector3 dir = _trans.TransformDirection(_moveDir);
-        //_controller.Move(dir * Time.deltaTime);
+        if (_moveDir.magnitude > 0.1f)
+        {
+            _targetRotation = Quaternion.LookRotation(_moveDir);
+            _trans.rotation = Quaternion.Slerp(_trans.rotation, _targetRotation, _rotateSpeed * Time.deltaTime);
+        }
     }
 
     private void RigidMove()
